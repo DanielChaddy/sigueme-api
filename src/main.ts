e7f58f14f047bae128a4aec5cce27c6ad5f4ec1c
Rene Polanco <renespolanco@hotmail.com>
2022-08-31 22:23:11 -0400

    Actualizacion..

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,9 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  const bodyParser = require('body-parser')
+  app.use(bodyParser.json({type: 'application/json', limit: '1mb'}))
   await app.listen(3000);
 }
 bootstrap();
